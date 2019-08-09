adf879889c24d5a250d3120e0c62b8467a141faf
Madhu Murthy <msmurthy@msn.com>
2019-08-08 18:09:27 -0700

    Create page.php

---
+++ page.php
@@ -0,0 +1,12 @@
+<?php
+  get_header();
+  if( have_posts() ):
+    while( have_posts() ) :
+      the_post(); ?>
+      <h1><?php the_title(); ?></h1>
+      <p><?php the_content(); ?></h1>
+    <?php endwhile;
+  endif;
+  get_sidebar();
+  get_footer();
+?>
